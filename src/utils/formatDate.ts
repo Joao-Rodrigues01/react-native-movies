cfc482478ecd51ab1a4745d293a4ad56b3bb8e3c
João Victor <j2003.victir@gmail.com>
2021-05-28 21:37:48 -0300

    add formatDate util

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,13 @@
+import { format  } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+export default function formatDate(date: string) {
+
+  const splitedDate = date.split('-');
+
+  const [ano, mes, dia] = splitedDate.map(data => Number(data))
+
+  const dateFormat = new Date(ano, mes - 1, dia);
+
+  return format(dateFormat, 'MMM dd, yyyy', { locale: ptBR });
+}
