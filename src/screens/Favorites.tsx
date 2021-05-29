fb67a41058871067e5273b6e4c55afd4fde36469
João Victor <j2003.victir@gmail.com>
2021-05-28 21:37:14 -0300

    add background color

--- src/screens/Favorites.tsx
+++ src/screens/Favorites.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Favorites() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Favorites</Text>
     </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#171821',
+    flex: 1,
+  }
+})
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Profile() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
     </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#171821',
+    flex: 1,
+  }
+})
