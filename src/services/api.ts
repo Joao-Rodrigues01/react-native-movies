135b152bca8dbe467ce09d505874095c8e611ac6
João Victor <j2003.victir@gmail.com>
2021-05-28 21:37:27 -0300

    add api calls

---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+export const popularMovies = {
+  url: `https://api.themoviedb.org/3/movie/popular?api_key=484e4f3139aad3bd78ac1031740719a8&language=en-US&page=1`
+};
+
+export const trendingTvShows = {
+  url: `https://api.themoviedb.org/3/trending/tv/week?api_key=484e4f3139aad3bd78ac1031740719a8&language=en-US`
+};
+
+
+export const watchListMovies = {
+  url: `https://api.themoviedb.org/3/account/10397196/watchlist/movies?api_key=484e4f3139aad3bd78ac1031740719a8&language=en-US&session_id=1cc121e2baccf7b5d492f6949cd1572030c97679&sort_by=created_at.asc&page=1`
+};
