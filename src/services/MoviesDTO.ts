745dbc1f5d59a8417967cada84f6cf378ee8b4b0
João Victor <j2003.victir@gmail.com>
2021-05-28 21:37:39 -0300

    add DTOs

---
+++ src/services/MoviesDTO.ts
@@ -0,0 +1,6 @@
+export interface MoviesDTO{
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
---
+++ src/services/TVShowDTO.ts
@@ -0,0 +1,6 @@
+export interface TVShowDTO{
+  id: number;
+  name: string;
+  poster_path: string;
+  first_air_date: string;
+}
