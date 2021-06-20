d38f78016f8698241df8a6fdae00ae361deb5bae
João Victor <j2003.victir@gmail.com>
2021-06-20 00:37:33 -0300

    add dimensions

--- src/components/CardMovie.tsx
+++ src/components/CardMovie.tsx
@@ -1,49 +1,86 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import { Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+	Text,
+	StyleSheet,
+	Image,
+	TouchableOpacity,
+	Dimensions,
+} from 'react-native';
 import formatDate from '../utils/formatDate';
 
 interface CardMovieProps {
-  title: string;
-  img: string;
-  date: string;
+	id: number;
+	title: string;
+	img: string;
+	date: string;
+	backgroundImg?: string;
+	overview?: string;
+	average?: number;
 }
 
-export default function CardMovie({ title, img, date }: CardMovieProps) {
-  const imageBaseURL =  'https://image.tmdb.org/t/p/w500';
+const deviceSize = Dimensions.get('window');
 
-  return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.75}>
-      <Image source={{ uri: `${imageBaseURL}/${img}` }} style={styles.img}/>
-      <Text numberOfLines={1}  ellipsizeMode="tail" style={styles.title}>{title}</Text>
-      <Text style={styles.date}>{formatDate(date)}</Text>
-    </TouchableOpacity>
-  )
+export default function CardMovie({
+	id,
+	title,
+	img,
+	date,
+	backgroundImg,
+	overview,
+	average,
+}: CardMovieProps) {
+	const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
+	const navigation = useNavigation();
+
+	function handleRoute() {
+		navigation.navigate('MovieDetails', {
+			id,
+			img,
+			title,
+			date,
+			backgroundImg,
+			overview,
+			average,
+		});
+	}
+
+	return (
+		<TouchableOpacity
+			onPress={handleRoute}
+			style={styles.container}
+			activeOpacity={0.75}
+		>
+			<Image source={{ uri: `${imageBaseURL}/${img}` }} style={styles.img} />
+			<Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>
+				{title}
+			</Text>
+			<Text style={styles.date}>{formatDate(date)}</Text>
+		</TouchableOpacity>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 8
-  },
-  title: {
-    color: '#fff',
-    fontSize: 16,
-    marginTop: 6,
-    paddingLeft: 8,
-    maxWidth: 140
-  },
-  img: {
-    width: 152,
-    height: 218,
-    borderRadius: 18      
-  },
-  date: {
-    color: '#686972',
-    marginTop: 2,
-    fontSize: 14,
-    paddingLeft: 8,
-    textTransform: 'capitalize'
-  }
-})
-
-
-
+	container: {
+		paddingHorizontal: 8,
+	},
+	title: {
+		color: '#fff',
+		fontSize: 16,
+		marginTop: 6,
+		paddingLeft: 8,
+		maxWidth: 140,
+	},
+	img: {
+		width: deviceSize.width * 0.4,
+		height: deviceSize.height * 0.34,
+		borderRadius: 20,
+	},
+	date: {
+		color: '#83848d',
+		marginTop: 2,
+		fontSize: 14,
+		paddingLeft: 8,
+		textTransform: 'capitalize',
+	},
+});
--- src/components/CardTVShow.tsx
+++ src/components/CardTVShow.tsx
@@ -1,18 +1,40 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import { Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 import formatDate from '../utils/formatDate';
 
 interface CardTVShowProps {
   name: string;
   img: string;
   date: string;
+  backgroundImg?: string;
+  overview?: string;
+  average?: number;
 }
 
-export default function CardTVShow({ name, img, date }: CardTVShowProps) {
+const deviceSize = Dimensions.get('window');
+
+
+export default function CardTVShow({ name, img, date, backgroundImg, average, overview }: CardTVShowProps) {
   const imageBaseURL =  'https://image.tmdb.org/t/p/w500';
+  const navigation = useNavigation();
+
+  function handleRoute() {
+    navigation.navigate('TVShowDetails', { 
+      name,
+      date,
+      backgroundImg,
+      average,
+      overview 
+    })
+  }
 
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.75}>
+    <TouchableOpacity 
+      style={styles.container} 
+      activeOpacity={0.75}
+      onPress={handleRoute}
+    >
       <Image source={{ uri: `${imageBaseURL}/${img}` }} style={styles.img}/>
       <Text numberOfLines={1}  ellipsizeMode="tail" style={styles.title}>{name}</Text>
       <Text style={styles.date}>{formatDate(date)}</Text>
@@ -30,18 +52,19 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: 6,
     paddingLeft: 8,
-    maxWidth: 130
+    maxWidth: 140
   },
   img: {
-    width: 152,
-    height: 218,
-    borderRadius: 18      
+    width: deviceSize.width * 0.40,
+    height: deviceSize.height * 0.34,
+    borderRadius: 20      
   },
   date: {
-    color: '#686972',
+    color: '#83848d',
     marginTop: 2,
     fontSize: 14,
     paddingLeft: 8,
     textTransform: 'capitalize'
   }
 })
+
